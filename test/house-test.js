323b2135cfdf9ba3c18770ef01ec1a5d7d718199
kiwi <kiwi.swhite.coder@gmail.com>
2013-01-25 15:22:32 +0800

    add player buy house

---
+++ test/house-test.js
@@ -0,0 +1,28 @@
+var vows = require('vows'),
+    assert = require('assert');
+
+var House = require('../lib/house.js').House;
+var Player = require('../lib/player.js').Player;
+
+vows.describe('player buy house').addBatch({
+
+	'should be success when player has enough money': function() {
+			var house = new House(100);
+			var player = new Player(1000);
+
+			player.buyHouse(house);
+
+			assert.equal(house.getOwner(), player);
+			assert.equal(player.getMoney(), 900);
+	},
+
+	'should receive exception when player does not have enough money': function() {
+			var house = new House(10000);
+			var player = new Player(1000);
+
+			assert.throws(function(){ player.buyHouse(house);}, Error);
+			assert.equal(player.getMoney(), 1000);
+			assert.isFalse(house.hasOwner());
+	}
+
+}).export(module);
