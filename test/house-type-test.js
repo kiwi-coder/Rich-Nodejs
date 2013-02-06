5f29e852ba293bf26dfec5a7a320fca9fa25df7c
kiwi <kiwi.swhite.coder@gmail.com>
2013-02-06 13:35:58 +0800

    add house type display

--- test/house-type-test.js
+++ test/house-type-test.js
@@ -2,27 +2,27 @@ var vows = require('vows'),
     assert = require('assert');
 
 var House = require('../lib/house.js').House;
-var Player = require('../lib/player.js').Player;
+var HouseType = require('../lib/house-type.js').HouseType;
 
 vows.describe('sell price').addBatch({
-    'should sell plat for 200 when the original price is 100 ': function() {
+    'should sell plat for 200 when the original price is 100 ':function () {
         var house = new House(100);
         assert.equal(house.getSellPrice(), 200);
     },
 
-    'should sell cottage for 400 when the original price is 100': function() {
+    'should sell cottage for 400 when the original price is 100':function () {
         var house = new House(100);
         house.setType('cottage');
         assert.equal(house.getSellPrice(), 400);
     },
 
-    'should sell villa for 600 when the original price is 100': function() {
+    'should sell villa for 600 when the original price is 100':function () {
         var house = new House(100);
         house.setType('villa');
         assert.equal(house.getSellPrice(), 600);
     },
 
-    'should sell skyscraper for 800 when the original price is 100': function() {
+    'should sell skyscraper for 800 when the original price is 100':function () {
         var house = new House(100);
         house.setType('skyscraper');
         assert.equal(house.getSellPrice(), 800);
@@ -57,5 +57,27 @@ vows.describe('visitor pay for toll fee').addBatch({
         house.setType("skyscraper");
 
         assert.equal(house.getToll(), 400);
+    }
+}).export(module);
+
+vows.describe('house type display').addBatch({
+    'should return 0 for plat':function () {
+        var plat = HouseType.createHouseType('plat');
+        assert.equal(plat.display(), '0');
+    },
+
+    'should return 1 for cottage':function () {
+        var cottage = HouseType.createHouseType('cottage');
+        assert.equal(cottage.display(), '1');
+    },
+
+    'should return 2 for villa':function () {
+        var villa = HouseType.createHouseType('villa');
+        assert.equal(villa.display(), '2');
+    },
+
+    'should return 3 for skyscraper': function() {
+        var skyscraper = HouseType.createHouseType('skyscraper');
+        assert.equal(skyscraper.display(), '3');
     }
 }).export(module);
