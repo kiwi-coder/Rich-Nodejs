5b46e409135022e54ea36f41bfa80980a14092c9
kiwi <kiwi.swhite.coder@gmail.com>
2013-02-20 15:48:48 +0800

    add site

---
+++ test/dummy-site-controller.js
@@ -0,0 +1,11 @@
+function DummySiteController() {
+    this.display = function () {
+        return '0';
+    };
+
+    this.acceptPlayer= function(player) {
+
+    };
+}
+
+exports.DummySiteController = DummySiteController;
